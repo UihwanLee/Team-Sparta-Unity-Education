3d77a5f1bc9ea2763a03028128a8bce532c40d71
Lee Ui Hwan <luh5063014@naver.com>
2025-09-03 15:17:13 +0900

    [RtanRain] Feat: 게임 엔딩 구현

--- RtanRain/Assets/Scripts/GameManager.cs
+++ RtanRain/Assets/Scripts/GameManager.cs
@@ -6,19 +6,40 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     [SerializeField] private GameObject rain;
+    [SerializeField] private GameObject endPanel;
     [SerializeField] private Text totalScoreTxt;
+    [SerializeField] private Text timeTxt;
 
-    int totalScore;
+    private int totalScore;
+    private float totalTime = 30.0f;
 
     private void Awake()
     {
         instance = this;
+        Time.timeScale = 1.0f;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating("MakeRain", 0f, 1.0f);
+        endPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(totalTime > 0f)
+        {
+            totalTime -= Time.deltaTime;
+        }
+        else
+        {
+            totalTime = 0f;
+            endPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        timeTxt.text = totalTime.ToString("N2");
     }
 
     public void MakeRain()

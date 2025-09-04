b6483d68b148f9860c14be21cd9a9025c45a11cd
Lee Ui Hwan <luh5063014@naver.com>
2025-09-04 14:53:13 +0900

    [DogVsCat] Feat: Cat HP 바 구현

---
+++ DogVsCat/Assets/Scripts/Cat.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    [SerializeField] private GameObject hungryCat;
+    [SerializeField] private GameObject fullCat;
+
+    [SerializeField] private RectTransform front;
+
+    private float full = 5.0f;
+    private float energy = 0.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+
+        float x = Random.Range(-9.0f, 9.0f);
+        float y = 30.0f;
+        transform.position = new Vector3(x, y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(energy <full)
+        {
+            transform.position += Vector3.down * 0.05f;
+        }
+        else
+        {
+            if(transform.position.x > 0)
+            {
+                transform.position += Vector3.right * 0.05f;
+            }
+            else
+            {
+                transform.position += Vector3.left * 0.05f;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Food"))
+        {
+            if (energy < full)
+            {
+                energy += 1.0f;
+                front.localScale = new Vector3(energy / full, 1.0f, 1.0f);
+                Destroy(collision.gameObject);
+                if(energy == full)
+                {
+                    hungryCat.SetActive(false);
+                    fullCat.SetActive(true);
+                }
+            }
+        }
+    }
+}
